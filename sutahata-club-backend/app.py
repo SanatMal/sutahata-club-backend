fd0aab39053f506541ba35e689b04414c9f9cf25
SanatMal <sanatmal100@gmail.com>
2025-07-13 18:49:38 +0530

    Update app.py

--- sutahata-club-backend/app.py
+++ sutahata-club-backend/app.py
@@ -25,4 +25,6 @@ def serve_static(filename):
 
 # Optional: For debugging on development server
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
